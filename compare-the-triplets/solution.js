52f71fa21206d71af6223b8254d2d56a88787a11
Nenad <nenadscsolid@gmail.com>
2018-01-03 18:20:47 +0000

    solution worth 10 points

---
+++ compare-the-triplets/solution.js
@@ -0,0 +1,56 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function solve(a0, a1, a2, b0, b1, b2){
+    // Complete this function
+    var points = {
+        a: 0,
+        b:0
+    };
+
+    checkPoints(a0, b0, points);
+    checkPoints(a1, b1, points);
+    checkPoints(a2, b2, points);
+
+    console.log(points.a, points.b);
+}
+
+function checkPoints(a, b, points) {
+    if (a > b) {
+        points.a++;
+    } else if (a < b) {
+        points.b++;
+    }
+}
+
+function main() {
+    var a0_temp = readLine().split(' ');
+    var a0 = parseInt(a0_temp[0]);
+    var a1 = parseInt(a0_temp[1]);
+    var a2 = parseInt(a0_temp[2]);
+    var b0_temp = readLine().split(' ');
+    var b0 = parseInt(b0_temp[0]);
+    var b1 = parseInt(b0_temp[1]);
+    var b2 = parseInt(b0_temp[2]);
+    var result = solve(a0, a1, a2, b0, b1, b2);
+    console.log(result.join(" "));
+}
