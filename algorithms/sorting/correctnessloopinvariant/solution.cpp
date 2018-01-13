7c85a3c7ae984016a75677ccf9896db9725aa7ca
Nenad <Neso@users.noreply.github.com>
2018-01-13 17:31:44 +0000

    loop invariant
    full solution

---
+++ algorithms/sorting/correctnessloopinvariant/solution.cpp
@@ -0,0 +1,46 @@
+/*
+* @Author: Nenad
+* @Date:   2018-01-13 17:30:51
+* @Last Modified by:   Nenad
+* @Last Modified time: 2018-01-13 17:31:05
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void insertionSort(int N, int arr[]) {
+    int i,j;
+    int value;
+    for(i=1;i<N;i++)
+    {
+        value=arr[i];
+        j=i-1;
+        while(j>=0 && value<arr[j])
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=value;
+    }
+    for(j=0;j<N;j++)
+    {
+        printf("%d",arr[j]);
+        printf(" ");
+    }
+}
+int main(void) {
+
+    int N;
+    scanf("%d", &N);
+    int arr[N], i;
+    for(i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(N, arr);
+
+    return 0;
+}
